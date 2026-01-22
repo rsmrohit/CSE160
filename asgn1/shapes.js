da693c392694cfae90987f2221efd2afde0467f9
rsmrohit <rohmam060405@gmail.com>
2026-01-22 12:50:47 -0800

    add

--- asgn1/shapes.js
+++ asgn1/shapes.js
@@ -22,6 +22,46 @@ class Point {
     }
 }
 
+// Custom Triangle class
+class TriangleCustom {
+    constructor(x1, y1, x2, y2, x3, y3, color, size) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
+        this.color = color;
+        this.size = size;
+    }
+
+    render() {
+        var rgba = this.color;
+        var s = this.size;
+
+        var vertices = new Float32Array([
+            this.x1, this.y1,
+            this.x2, this.y2,
+            this.x3, this.y3
+        ]);
+
+        var n = 3;
+        var vertexBuffer = gl.createBuffer();
+        if (!vertexBuffer) {
+            console.log('Failed to create buffer');
+            return;
+        }
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+        gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(a_Position);
+
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+        gl.drawArrays(gl.TRIANGLES, 0, n);
+    }
+}
+
 // Triangle class
 class Triangle {
     constructor(x, y, color, size) {
